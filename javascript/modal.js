cfb5931d6daddc29399e62e71186ba7bc1fef530
Vyplasil Nicolas <nico.vyp@hotmail.fr>
2022-08-25 14:55:32 +0400

    creation  des variables du formulaire

--- javascript/modal.js
+++ javascript/modal.js
@@ -7,23 +7,31 @@ function editNav() {
   }
 }
 
-// DOM Elements
+/* DOM Elements */
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
-const closeBtn = document.querySelectorAll(".close");
+/* variables du formulaire */
+const formData = document.querySelectorAll(".formData"); 
+const closeBtn = document.querySelectorAll(".close"); 
+const form = document.querySelector("form");
+const first = document.getElementById("first");
+const last = document.getElementById("last");
+const email = document.getElementById("email");
+const birthdate = document.getElementById("birthdate");
+const quantity = document.getElementById("quantity");
+const radios = document.querySelectorAll(".checkbox-input[type=radio]");
+const conditionsGenerales = document.getElementById("checkbox1");
+const btnSubmit = document.querySelector(".btn-submit");
+const btnSignUp = document.querySelector(".btn-signup");
 
-// launch modal event
+
+/*  évèvement par le click d'ouverture et de fermeture de la modal  */
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 closeBtn.forEach((btn) => btn.addEventListener("click", closeModal));
-
-
-// launch modal form
+/* lancement de la modal formulaire   */
 function launchModal() {
-  modalbg.style.display = "block";
-}
-
-// close modal form
+  modalbg.style.display = "block";}
+/* fermeture de la modal formulaire   */
 function closeModal() {
   modalbg.style.display = "none";
 }
