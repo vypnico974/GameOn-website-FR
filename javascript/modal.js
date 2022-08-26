d975488d67d2ffcae8091fc177724c04dc73c70a
Vyplasil Nicolas <nico.vyp@hotmail.fr>
2022-08-26 14:20:21 +0400

    fin de création des fonctions de validations des saisies

--- javascript/modal.js
+++ javascript/modal.js
@@ -31,7 +31,7 @@ const messagesError = {
   birthdateError: 'Vous devez entrer votre date de naissance.',
   quantityError: 'Veuillez entrer un nombre de tournoi.',
   locationError: 'Veuillez choisir un tournoi.',
-  cguError: "Vous devez vérifier que vous acceptez les termes et conditions.",
+  useError: "Vous devez vérifier que vous acceptez les termes et conditions.",
 };
 
 
@@ -49,32 +49,26 @@ function closeModal() {
 }
 
 
-
 /* fonction pour vérifier tous les champs du formulaire */
 function validateInputs() {
   validateFirst(); 
-  validateLast();   
-  /*
-  
+  validateLast(); 
   validateEmail();
+  validateBirthdate();
   validateQuantity();
   validateLocation();
   validateConditions();
-  validateBirthdate(); */
 }
-
-
-
 
 // vérification du prénom 
 function validateFirst() {
   const firstLength = first.value.length;
   /* expressions régulières(regex) 
   /^ pour début   $/ pour fin  
-  [A-zÀ-ú- .'-] les lettres minuscule et majuscule avec accent possible et
-   espace tiret apostrophe sont autorisés
-   {2,} minimun 2 autorisés                  */
-  const regex = /^[[A-zÀ-ú- ,.'-]{2,}$/;
+  [A-zÀ-ú-] les lettres minuscule et majuscule avec accent possible et
+   ainsi que tiret sont autorisés
+   {2,} minimun 2 autorisés               */
+  const regex = /^[A-zÀ-ú'-]{2,}$/;
   if (regex.test(first.value) === false) {
     // affichage du message d'erreur pour le prénom 
     document.querySelector('.first-error').innerText =
@@ -88,15 +82,14 @@ function validateFirst() {
 
 // vérification du nom 
 function validateLast() {
-  const firstLength = last.value.length;
-  /* expressions régulières(regex) 
-  /^ pour début   $/ pour fin  
-  [A-zÀ-ú- .'-] les lettres minuscule et majuscule avec accent possible et
-   espace tiret apostrophe sont autorisés
-   {2,} minimun 2 autorisés                  */
-  const regex = /^[[A-zÀ-ú- ,.'-]{2,}$/;
+  const firstLength = last.value.length; 
+  /* expressions régulières(regex)  /^ pour début   $/ pour fin   
+  [A-zÀ-ú- '] les lettres minuscule et majuscule avec accent possible et
+   ainsi que tiret apostrophe espace sont autorisés
+   {2,} minimun 2 autorisés */               
+  const regex = /^[A-zÀ-ú-' ]{2,}$/;
   if (regex.test(last.value) === false) {
-    // affichage du message d'erreur pour le nom 
+    /* affichage du message d'erreur pour le nom */
     document.querySelector('.last-error').innerText =
     messagesError.firstNameError;
     return false;
@@ -106,11 +99,82 @@ function validateLast() {
   return true;
 }
 
+/*  vérification format email   */
+function validateEmail() {
+  /* caractère alphanumerique (sans accent) avant et après le "@" et le point  */
+  const regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,}$/;
+  if (regex.test(email.value) === false) {
+    /* affichage du message d'erreur pour email */
+    document.querySelector('.email-error').innerText = messagesError.emailError;
+    return false;
+  }
+  /* pas de message d'erreur qui s'affichage  */
+  document.querySelector('.email-error').innerText = '';
+  return true;
+}
 
+/* Vérification date de naissance */
+function validateBirthdate() {
+  /* AAAA-MM-JJ  */
+  const regex = /^[0-9]{4}-[0-9]{2}-[0-9]{2}$/;
+  if (regex.test(birthdate.value) === false) {
+    /* affichage du message d'erreur pour date de naissance */
+    document.querySelector('.birthdate-error').innerText =
+    messagesError.birthdateError;
+    return false;
+  }
+  /* pas de message d'erreur qui s'affichage  */
+  document.querySelector('.birthdate-error').innerText = '';
+  return true;
+}
 
-// Ecoute le submit du formulaire et test du prénom
+// Checks if the user has selected a valid quantity
+function validateQuantity() {
+  /* 0 à 99 */
+  const regex = /^[0-9]{1,2}$/;
+  if (regex.test(quantity.value) === false) {
+    /* affichage du message d'erreur pour tournoi */
+    document.querySelector('.quantity-error').innerText =
+      messagesError.quantityError;
+    return false;
+  }
+  /* pas de message d'erreur qui s'affichage  */
+  document.querySelector('.quantity-error').innerText = '';
+  return true;
+}
+
+/* vérification qu'une location est sélectionnée */
+function validateLocation() {
+  for (let radio of radios) {
+    if (radio.checked === true) {
+      /* pas d'affichage du message d'erreur location*/
+      document.querySelector('.location-error').innerText = '';
+      return true;
+    }
+  }
+  /* affichage du message d'erreur location*/
+  document.querySelector('.location-error').innerText =
+    messagesError.locationError;
+  return false;
+}
+
+// Vérification Conditions d'utilisation
+function validateConditions() {
+  if (conditionsGenerales.checked === true) {
+     /* pas d'affichage du message d'erreur conditions d'utilisation*/
+    document.querySelector('.use-error').innerText = '';
+    return true;
+  }
+  /* affichage du message d'erreur conditions d'utilisation*/
+  document.querySelector('.use-error').innerText = messagesError.useError;
+  return false;
+}
+
+// évènement du submit formulaire et vérifications saisies du formulaire
 form.addEventListener('submit', (e) => { 
-   e.preventDefault();  /* ne pas recharger la modale avec le click submit */
-  validateInputs();
- 
-});
+    e.preventDefault();  /* ne pas recharger la modale avec le click submit */
+    validateInputs();
+   
+  });
+
+
